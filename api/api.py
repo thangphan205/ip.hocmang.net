aafd793006d9d21d1fa7cfdce0cc7764293c961c
Thang Phan <thangphan205@gmail.com>
2020-06-10 17:41:01 +0700

    add ping traceroute

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+import subprocess
 
 api_router = APIRouter()
 
@@ -7,3 +8,25 @@ api_router = APIRouter()
 def read_ip(request: Request):
     ip = request.client.host
     return {"ipv4": ip}
+
+
+@api_router.get("/ping")
+def ping(hostname: str, request: Request):
+    ip = request.client.host
+    result = subprocess.run(
+        ["ping", hostname, "-c4"], stdout=subprocess.PIPE, universal_newlines=True
+    )
+    if result.returncode == 0:
+        return {"ipv4": ip, "result": result.stdout}
+    return {"ipv4": ip, "result": ""}
+
+
+@api_router.get("/traceroute")
+def traceroute(hostname: str, request: Request):
+    ip = request.client.host
+    result = subprocess.run(
+        ["traceroute", hostname, "-n"], stdout=subprocess.PIPE, universal_newlines=True,
+    )
+    if result.returncode == 0:
+        return {"ipv4": ip, "result": result.stdout}
+    return {"ipv4": ip, "result": ""}
